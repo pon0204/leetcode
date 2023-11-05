d5d8bbe4dbcb8c6b23c0808398d6de6a95cac81d
= <=>
2023-11-05 14:56:44 +0900

    first commit

---
+++ 217.contains-duplicate.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=217 lang=typescript
+ *
+ * [217] Contains Duplicate
+ */
+
+// @lc code=start
+function containsDuplicate(nums: number[]): boolean {
+  const set = new Set(nums)
+  return set.size !== nums.length
+}
+// @lc code=end
